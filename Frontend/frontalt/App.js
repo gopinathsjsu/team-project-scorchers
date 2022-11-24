02547cf200c509322efab111c0acd53fa957a6a1
prithvibunny <nvpr99@gmail.com>
2022-11-24 00:32:58 -0800

    commit

---
+++ Frontend/frontalt/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import LandingPage from './components';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import SearchFlights from './components/SearchFlights';
+import FlightDetails from './components/FlightDetails';
+import ViewBookings from './components/Bookings/ViewBookings';
+import ViewFlights from './components/Bookings/ViewFlights';
+import CustomerProfile from './components/Profile/CustomerProfile';
+import EmployeeProfile from './components/Profile/EmployeeProfile';
+import Payment from './components/Payment';
+import Airline from './components/Airline';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/Signup" component={Signup} />
+      <Route exact path="/Login" component={Login} />
+      <Route exact path="/SearchFlights" component={SearchFlights} />
+      <Route exact path="/FlightDetails" component={FlightDetails} />
+      <Route exact path="/Airline" component={Airline} />
+      <Route exact path="/CustomerProfile" component={CustomerProfile} />
+      <Route exact path="/EmployeeProfile" component={EmployeeProfile} />
+      <Route exact path="/Bookings" component={ViewBookings} />
+      <Route exact path="/ViewFlights" component={ViewFlights} />
+      <Route exact path="/Payment" component={Payment} />
+    </>
+  );
+}
+
+export default App;
