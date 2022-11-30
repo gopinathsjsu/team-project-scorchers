786546a3a3dc70742b133ca11859e2804aab97ae
Shanmukh459 <saikrrish459@gmail.com>
2022-11-30 15:39:52 -0800

    Home page heading and allowed apps tweak to deploy

--- airport_management_system/airport_management_system/settings.py
+++ airport_management_system/airport_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mbkulj+-)fcj^k69l=cr*=!i5u(l)zi%jnbd9g)=$rmc$3qycz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
